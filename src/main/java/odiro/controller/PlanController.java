50af98f903a98bd7e1ca67ffebd54c3707245762
KimJeongHyeon <sbc45672@naver.com>
2024-07-14 20:14:33 +0900

    Fix: 버그 수정
    api 수정

--- src/main/java/odiro/controller/PlanController.java
+++ src/main/java/odiro/controller/PlanController.java
@@ -33,7 +33,7 @@ import java.util.stream.Collectors;
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-//@RequestMapping("/api")
+@RequestMapping("/api")
 public class PlanController {
 
     private final PlanService planService;
