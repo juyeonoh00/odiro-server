90430881994344ca1d9633d62b6730e3a89666c2
DESKTOP-TAAR968\sbc45 <sbc45672@naver.com>
2024-11-13 02:53:32 +0900

    Fix : 친구추가 컨트롤러 오류 수정

--- backend/src/main/java/odiro/controller/FriendRequestController.java
+++ backend/src/main/java/odiro/controller/FriendRequestController.java
@@ -15,13 +15,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import odiro.repository.FriendRequestRepository;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
-@Controller
+@RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
 public class FriendRequestController {
